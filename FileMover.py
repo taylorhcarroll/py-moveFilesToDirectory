7e7afb565ff5b01bbda6703030d363a820ac9ab9
taylorhcarroll <taylorhcarroll1@gmail.com>
2022-05-10 22:28:16 -0500

    another

--- FileMover.py
+++ FileMover.py
@@ -9,9 +9,16 @@ import time
 class MyHandler(FileSystemEventHandler):
     i = 1
     def on_modified(self, event):
+        new_name = "new_file_" + str(self.i) + ".txt"
         for filename in os.listdir(folder_to_track):
+            file_exists = os.path.isfile(folder_destination + "/" + new_name)
+            while file_exists:
+                self.i += 1
+                new_name = "new_file_" + str(self.i) + ".txt"
+                file_exists = os.path.isfile(folder_destination + "/" + new_name)
+
             src = folder_to_track + "/" + filename
-            new_destination = folder_destination + "/" + filename
+            new_destination = folder_destination + "/" + new_name
             os.rename(src, new_destination)
 
 folder_to_track = "/home/pi/Desktop/myFolder"
