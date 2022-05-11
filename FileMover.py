1f5e5b039349bb8f745a5db92cc182eae3f66d6a
taylorhcarroll <taylorhcarroll1@gmail.com>
2022-05-10 21:42:05 -0500

    initial

---
+++ FileMover.py
@@ -0,0 +1,28 @@
+from watchdog.observer import observer
+from watchdog.events import FileSystemEventHandler
+# pip install watchdog for above packages to work
+
+import os
+import json
+import time
+
+class MyHandler(FileSystemEventHandler):
+    i = 1
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + filename
+            os.rename(src, new_destination)
+
+folder_to_track = "/home/pi/Desktop/myFolder"
+folder_destination = "/home/pi/Desktop/newFolder"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+
+try: 
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
